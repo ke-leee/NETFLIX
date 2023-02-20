0953efc7d6a8330f21f2ed896c0099c2bff4f05f
ke-leee <lkee0722@naver.com>
2023-02-20 18:24:43 +0900

    add home 1

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,6 +1,11 @@
-const Home = () => {
+import { useQuery } from "react-query";
+import { getMovies } from "../api";
+
+function Home() {
+  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
+  console.log(data, isLoading);
   return (
     <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>home</div>
   );
-};
+}
 export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,20 @@ import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./components/GlobalStyle";
 import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const client = new QueryClient();
 root.render(
   <RecoilRoot>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </RecoilRoot>
 );
